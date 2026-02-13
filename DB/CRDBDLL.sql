42a126373298e5fc3936124c46e39db7b98fe3b3
RomanGlinyanko <roman.glinyanko@gmail.com>
2026-02-13 16:22:49 +0300

    + Скрипт создания БД

---
+++ DB/CRDBDLL.sql
@@ -0,0 +1,13 @@
+create database quest encoding utf8;
+create schema quest authorization quest;
+create user quest with password 'RX777ngS';
+
+/* select 
+	* 
+from 
+	pg_roles r
+join
+	pg_user u
+on
+	r.rolname = u.usename;
+*/
