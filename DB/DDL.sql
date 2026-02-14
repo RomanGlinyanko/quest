6cf7acc407cd727ae39b27c04d75403aae7376b7
RomanGlinyanko <roman.glinyanko@gmail.com>
2026-02-14 12:09:48 +0300

    Добавлены DDL, DELDDL и DML

---
+++ DB/DDL.sql
@@ -0,0 +1,8 @@
+-- Таблицы
+create table quest
+(
+	id bigserial primary key,
+	quest text
+);
+-- select * from quest
+-- select * from information_schema.tables
---
+++ DB/DML.sql
@@ -0,0 +1,3 @@
+insert into quest (quest) values ('
+Что такое SOLID
+');
